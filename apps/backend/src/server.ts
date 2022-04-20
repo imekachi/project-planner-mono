829c76370d79de3d354958e48f4513da2ab1bc57
imekachi <imekachi@gmail.com>
2022-04-20 20:15:56 +0700

    feat(backend): add chalk for terminal log color

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import { resolvers } from '@generated/type-graphql'
 import { ApolloServer } from 'apollo-server'
+import chalk from 'chalk'
 import * as tq from 'type-graphql'
 import { context } from './context'
 
@@ -12,11 +13,11 @@ const app = async () => {
   const server = new ApolloServer({ context, schema })
 
   server.listen({ port: process.env.API_PORT }).then(({ url }) => {
-    console.log(`==== 🚀 Server ready ====`)
+    console.log(chalk.green(`==== 🚀 Server ready ====`))
     console.log(
       `
-GraphQL API : ${url}graphql
-Health check: ${url}.well-known/apollo/server-health
+GraphQL API : ${chalk.blueBright(`${url}graphql`)}
+Health check: ${chalk.blueBright(`${url}.well-known/apollo/server-health`)}
     `.trim()
     )
   })
