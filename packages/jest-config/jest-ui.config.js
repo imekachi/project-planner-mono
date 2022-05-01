ad4ee0fcdad19e67067aebcdf581b4c3715e7def
imekachi <imekachi@gmail.com>
2022-05-01 15:45:04 +0700

    chore: upgrade dependencies and set up tests in ui-react

--- packages/jest-config/jest-ui.config.js
+++ packages/jest-config/jest-ui.config.js
@@ -1,4 +1,7 @@
 const jestBaseConfig = require('./jest-base.config')
+
 module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['jest-config/jest-ui.setup.ts'],
   ...jestBaseConfig,
 }
