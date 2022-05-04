da810b69793d1d76639fa7c7e852c2c9043b298c
imekachi <imekachi@gmail.com>
2022-05-04 21:05:41 +0700

    feat(type-utils): add Required<T, K> and RequiredAll<T>

--- packages/type-utils/index.ts
+++ packages/type-utils/index.ts
@@ -1,2 +1,16 @@
 export type UnknownObject = Record<string, unknown>
+
+/**
+ * Make some keys optional
+ */
 export type Optional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: T[SubKey] }
+
+/**
+ * Require some keys
+ */
+export type Required<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]-?: T[SubKey] }
+
+/**
+ * Require all keys, basically the built-in Require<T>
+ */
+export type RequiredAll<T> = { [K in keyof T]-?: T[K] }
