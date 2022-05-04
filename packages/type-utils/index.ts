b6ef38c715f39734d38140849549c0795d89c2ca
imekachi <imekachi@gmail.com>
2022-05-04 22:01:07 +0700

    feat(type-utils): add RequiredOnly<T, K> to require some keys and make others optional

--- packages/type-utils/index.ts
+++ packages/type-utils/index.ts
@@ -11,6 +11,11 @@ export type Optional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: T[Su
 export type Required<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]-?: T[SubKey] }
 
 /**
+ * Require only the specified keys and make the rest optional
+ */
+export type RequiredOnly<T, K extends keyof T> = Required<Partial<T>, K>
+
+/**
  * Require all keys, basically the built-in Require<T>
  */
 export type RequiredAll<T> = { [K in keyof T]-?: T[K] }
