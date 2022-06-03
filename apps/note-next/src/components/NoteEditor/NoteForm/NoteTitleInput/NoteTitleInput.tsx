9b4551753df1b0a535db9a806f3b9fa4207c0678
imekachi <imekachi@gmail.com>
2022-06-03 20:30:13 +0700

    feat(note-next): add skeleton loading style to title input

--- apps/note-next/src/components/NoteEditor/NoteForm/NoteTitleInput/NoteTitleInput.tsx
+++ apps/note-next/src/components/NoteEditor/NoteForm/NoteTitleInput/NoteTitleInput.tsx
@@ -3,18 +3,23 @@ import { ChangeEvent } from 'react'
 export type NoteTitleInputProps = {
   value: string
   onChange: (newValue: string) => void
+  showLoader?: boolean
 }
 
 const NoteTitleInput = (props: NoteTitleInputProps): JSX.Element => {
-  const { value, onChange } = props
+  const { value, onChange, showLoader } = props
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.value)
   }
 
+  if (showLoader) {
+    return <div className="skeleton input-field w-full text-lg" />
+  }
+
   return (
     <input
-      className="mb-4 w-full text-lg font-bold focus-visible:outline-0"
+      className="w-full text-lg font-bold focus-visible:outline-0"
       name="title"
       type="text"
       placeholder="Title"
